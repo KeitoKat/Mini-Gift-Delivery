fba2372f38c9979fbf4741b534e8ab0d2f3845af
KeitoKat <khammyschl@gmail.com>
2021-05-18 22:58:03 +0700

    commit on "changes"

--- mini-gift-delivery/src/components/CheckoutForm/PaymentForm.jsx
+++ mini-gift-delivery/src/components/CheckoutForm/PaymentForm.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Button, Divider  } from "@material-ui/core";
+import { Elements, CardElement, ElementsConsumer } from "@stripe/react-stripe-js";
+import { loadStrip } from "@stripe/stripe-js";
 
 const PaymentForm = () => {
   return (
